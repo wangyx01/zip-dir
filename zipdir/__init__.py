31a60023c1b69017971715b6e4ba0199492c392c
wangyx64 <wangyx64@lenovo.com>
2021-01-18 13:37:39 -0500

    init

--- zipdir/__init__.py
+++ zipdir/__init__.py
@@ -5,6 +5,13 @@ import pathlib
 
 
 def zip_directory(directory, output=None, compression=zipfile.ZIP_STORED):
+    """
+        directory: A directory that will be the root directory of the archive; ie. we typically
+                chdir into 'directory' before creating the archive.
+        output: The name of the file to create, the name suffix is ".zip".
+        compression: zipfile.ZIP_STORED (no compression), zipfile.ZIP_DEFLATED (requires zlib),
+                 zipfile.ZIP_BZIP2 (requires bz2) or zipfile.ZIP_LZMA (requires lzma).
+    """
     save_cwd = os.getcwd()
     if not os.path.isdir(directory):
         raise Exception(f"{directory} not a directory")
